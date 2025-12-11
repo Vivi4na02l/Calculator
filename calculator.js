ed275dccc317290c5eccbee4a0be33ace805e3af
Viviana <40200339@esmad.ipp.pt>
2025-12-11 19:15:29 +0000

    equation's symbols are now saved in "equationSymbols" array

--- calculator.js
+++ calculator.js
@@ -5,19 +5,28 @@ let symbolTyped = false;
 let oldScreen;
 
 let number = '';
+let symbol;
 let numbers = [];
 let equationSymbols = [];
 
+/**
+ * when any of the numbers' buttons are clicked...
+ */
 for (const btnNumber of btnsNumber) {
     btnNumber.addEventListener("click", () => {
         addOnScreen(btnNumber.innerHTML);
 
         if (symbolTyped) {
             symbolTyped = false;
+            equationSymbols.push(symbol);
         }
     })
 }
 
+/**
+ * adds number to the screen visually
+ * @param {*} nbr the number of the button clicked
+ */
 function addOnScreen(nbr) {
     number += nbr;
 
@@ -32,7 +41,7 @@ function addOnScreen(nbr) {
 
 /**
  * adds symbol to the screen and allows the user to switch between symbols before adding a new number to the screen
- * @param {txtSymbol} txtSymbol the symbol as a text to be added visually on the screen
+ * @param {*} txtSymbol the symbol as a text to be added visually on the screen
  * @param {*} equation the actual meaning of the symbol that is used logically
  */
 function equationSymbol(txtSymbol, equation) {
@@ -43,7 +52,6 @@ function equationSymbol(txtSymbol, equation) {
             // if a symbol hasn't yet been clicked after the last number...
             if (!symbolTyped) {
                 numbers.push(number);
-                console.log(numbers);
 
                 oldScreen = screen.innerHTML; // registers the screen.innerHTML before adding the symbol
         
@@ -51,6 +59,7 @@ function equationSymbol(txtSymbol, equation) {
             }
         
             if (symbolTyped) {
+                symbol = equation;
                 screen.innerHTML = oldScreen; // makes the screen.inner be equal to what it was before symbols were added after the last number
                 screen.innerHTML += txtSymbol; // adds symbol
             }
